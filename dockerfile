f5f70403fff32c1e8bc8c5ea2c15e51aca39105f
hosnahoseini <hosna.hoseini@gmail.com>
2022-07-23 09:04:53 +0430

    fix docker copmpose + add volume

--- dockerfile
+++ dockerfile
@@ -2,12 +2,13 @@ FROM python:3.8
 
 RUN mkdir -p /home/app
 
+COPY requirements.txt /home/app/requirements.txt
 
 RUN apt-get update
 RUN apt-get install ffmpeg libsm6 libxext6  -y
-RUN pip install -r /home/app/requirements.txt
 
-COPY . /home/app
+
+RUN pip install -r /home/app/requirements.txt
 
 EXPOSE 8001
 CMD ["python", "/home/app/main.py"] 
--- src/main.py
+++ src/main.py
@@ -3,13 +3,16 @@ import sys
 import cv2
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
+import io
 import numpy as np
+from starlette.responses import StreamingResponse
 
+PATH = "/home/app/"
 app = FastAPI()
 
 
 def detect(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
 
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     faces = faceCascade.detectMultiScale(
@@ -23,9 +26,9 @@ def detect(image):
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         roi_color = image[y:y + h, x:x + w]
-        # cv2.imwrite(str(w) + str(h) + '_faces.jpg', roi_color)
+        # cv2.imwrite(str(w) + str(h) + '_faces.png', roi_color)
     
-    status = cv2.imwrite('faces_detected.jpg', image)
+    status = cv2.imwrite(PATH + 'faces_detected.png', image)
     return(faces.tolist())
 
 @app.get("/path/{imagePath:path}")
@@ -39,6 +42,13 @@ async def detect_with_file(file: UploadFile = File(...)):
     nparr = np.fromstring(contents, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return detect(img)
+
+@app.get("/last_output/")
+async def image_endpoint():
+    # Returns a cv2 image array from the document vector
+    cv2img = cv2.imread(PATH + "faces_detected.png")
+    res, im_png = cv2.imencode(".png", cv2img)
+    return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
 
 # python main.py
 if __name__ == '__main__':
